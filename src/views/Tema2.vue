c7038f74e07a517d295ba2902852b53a8482665e
ricardog07 <71521082+ricardog07@users.noreply.github.com>
2021-07-08 12:54:04 -0500

    Se actualiza la base.

---
+++ src/views/Tema2.vue
@@ -0,0 +1,95 @@
+<template lang="pug">
+.curso-main-container.pb-3
+  BannerInterno
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5
+    
+    .titulo-principal
+      .titulo-principal__numero
+        span 2
+      h1 Tipos de mercadeo
+
+    p.mb-5 Hay diversos tipos de mercadeo, que se direccionan y se mezclan según la intención de la estrategia de las organizaciones. A continuación, se pueden observar los más conocidos. #[strong ¿Cuáles son? ¿Qué características tienen? ¿Cuál de todos elegir para la vida laboral?] Las siguientes tarjetas pueden ayudar a la identificación de las respuestas.
+
+    .row
+      .col-lg-4.mb-5
+        .tarjeta.color_tar.p-4
+          .h4.text-center.mt-5 Directo
+          p.mb-4.text-center Basado en la comunicación 
+            br
+            | directa y bidireccional. Además, 
+            br
+            | 
+            strong una audiencia determinada  
+            br 
+            | 
+            strong y bien segmentada. 
+          img.img-a(src='@/assets/curso/tema_2/img_1.png' alt='AvatarTop')
+
+      .col-lg-4.mb-5
+        .tarjeta.color-secundario.p-4
+          .h4.text-center.mt-5 Online
+          p.mb-5.text-center Basado en las acciones y 
+            br
+            |
+            strong contenidos a través del 
+            br
+            | 
+            strong internet.
+          img.img-a(src='@/assets/curso/tema_2/img_2.png' alt='AvatarTop')
+
+      .col-lg-4.mb-5
+        .tarjeta.color_tar.p-4
+          .h4.text-center.mt-5 De nicho
+          p.mb-4.text-center Basado en la satisfacción y 
+            br
+            | resolución de problemas 
+            br
+            | de #[strong un grupo poblacional] 
+            br
+            |
+            strong específico.
+          img.img-a(src='@/assets/curso/tema_2/img_3.png' alt='AvatarTop')
+
+    .row.justify-content-center
+      .col-lg-4.mb-5
+        .tarjeta.color-secundario.p-4.t-ava
+          .h4.text-center.mt-5 De influencia
+          p.mb-4.text-center Basado en #[strong las redes]
+            br
+            | 
+            strong sociales y las celebridades 
+            br
+            | 
+            strong para incidir en la toma de 
+            br
+            | #[strong decisiones] de las compras 
+            br
+            | de las personas. 
+          img.img-a(src='@/assets/curso/tema_2/img_4.png' alt='AvatarTop')
+
+      .col-lg-4.mb-5
+        .tarjeta.color_tar.p-4
+          .h4.text-center.mt-5 Masivo
+          p.mb-5.text-center Busca y logra alcanzar 
+            br
+            | grupos grandes 
+            br
+            |
+            strong poblacionales sin tener 
+            br
+            |
+            strong en cuenta el nicho. 
+          img.img-a(src='@/assets/curso/tema_2/img_5.png' alt='AvatarTop')
+
+</template>
+
+<script>
+export default {
+  name: 'Tema2',
+  data: () => ({
+    // variables de vue
+  }),
+}
+</script>
+
+<style lang="sass" scoped></style>
---
+++ src/views/Tema6.vue
@@ -0,0 +1,117 @@
+<template lang="pug">
+.curso-main-container.pb-3
+  BannerInterno
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5
+    
+    .titulo-principal
+      .titulo-principal__numero
+        span 6
+      h1 Análisis de competencia
+
+    p.mb-5 Desde el mercadeo, se plantea que, para el éxito empresarial, #[strong es necesario proporcionar mayor valor y satisfacción al cliente que lo que pueden generar los competidores dentro de una industria o mercado]. Analizar la competencia es el proceso de generar ventaja estratégica competitiva, mediante el proceso de posicionar la propuesta de valor en la mente de los consumidores, destacándose en comparación con la oferta de los competidores.
+
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-3
+        img.img-a(src='@/assets/curso/tema_6/img_1.png')
+      .col-lg-6
+        .cajon--amarillo.p-5
+          p En el marco de las dinámicas de mercado, #[strong evidenciar y entender las fortalezas y debilidades de la competencia es el mayor reto y brindará elementos para generar propuestas de diferenciación estratégica], entendiendo la posición competitiva de la marca o productos en comparación con los competidores.
+
+    .titulo-segundo
+      .h4 Dimensiones en el análisis de la competencia
+    p.mb-5 El punto de partida para el análisis de la competencia es el análisis comparativo de la propuesta de valor, con sus características y beneficios del producto. Esto posibilita #[strong identificar los elementos diferenciadores frente a productos similares de otras empresas], teniendo claro que es importante generar diferencias racionales, como son las características del precio, y emocionales, como el significado de la marca en la vida de los consumidores. Para realizar el análisis de los competidores, hay que tener en cuenta las siguientes dimensiones:
+
+    .row.align-items-center
+      .col-lg-2
+        img.img-a(src='@/assets/curso/tema_6/img_2.svg')
+      .col-lg-10
+        .h5 Elementos relacionados con el posicionamiento de la marca
+        p Posicionar es generar una imagen de la marca, producto, o servicio en las mentes de los consumidores. Se construye desde la percepción que tiene el consumidor individualmente con respecto a la competencia. Debe estar basado en elementos tales como diferenciación, beneficios relevantes, inversiones rentables, comunicación clara y procesos de perfeccionamiento, con los que se incrementa cada vez más el valor añadido, generando ventajas competitivas.
+    hr.hr
+    .row.align-items-center
+      .col-lg-2
+        img.img-a(src='@/assets/curso/tema_6/img_3.svg')
+      .col-lg-10
+        .h5 Elementos de las ventas y la participación en el mercado
+        p Consisten en el porcentaje de ventas y comercialización de un producto o servicio, y son el resultado de la estrategia de mercadeo, donde se identifican precios de comercialización, estrategias de publicidad, distribución, descuentos, identificando zonas geográficas fuertes y débiles, en comparación con los competidores.
+    hr.hr
+    .row.align-items-center
+      .col-lg-2
+        img.img-a(src='@/assets/curso/tema_6/img_4.svg')
+      .col-lg-10
+        .h5 Elementos de visibilidad y publicidad
+        p Se busca identificar la inversión publicitaria y el ruido, es decir, volumen y cantidad de dicha publicidad en términos de frecuencia y alcance del mensaje.
+    hr.hr
+    .row.align-items-center
+      .col-lg-2
+        img.img-a(src='@/assets/curso/tema_6/img_5.svg')
+      .col-lg-10
+        .h5 Elementos de ventajas y características competitivas
+        p Identificar los beneficios y características de los productos de nuestros competidores, para generar superioridad sobre ellos y que dichas características aíslen a nuestra empresa de dichos competidores.
+    hr.hr
+    .row.align-items-center
+      .col-lg-2
+        img.img-a(src='@/assets/curso/tema_6/img_6.svg')
+      .col-lg-10
+        .h5 Elementos de costo, rentabilidad y eficiencia logística
+        p Analizar toda la estructura de costos de los competidores, su logística, y la eficiencia. Estos elementos pueden ser ventaja o barrera competitiva.
+    hr.hr
+    .row.align-items-center
+      .col-lg-2
+        img.img-a(src='@/assets/curso/tema_6/img_7.svg')
+      .col-lg-10
+        .h5 Elementos de precio
+        p Para la estrategia de precios, es necesario conocer los precios, descuentos  y las condiciones de negociación de la competencia.
+    hr.hr
+    .row.align-items-center
+      .col-lg-2
+        img.img-a(src='@/assets/curso/tema_6/img_8.svg')
+      .col-lg-10
+        .h5 Elementos comunicativos
+        p Identificar la estrategia comunicativa, el target, elementos de la promesa de valor y todos aquellos elementos comunicativos diferenciadores de la marca. 
+    hr.hr
+    .row.align-items-center
+      .col-lg-2
+        img.img-a(src='@/assets/curso/tema_6/img_9.svg')
+      .col-lg-10
+        .h5 Elementos de satisfacción del cliente
+        p Establecer benchmarks para identificar indicadores de satisfacción de los clientes propios y de la competencia. 
+    hr.hr
+    .row.align-items-center
+      .col-lg-2
+        img.img-a(src='@/assets/curso/tema_6/img_10.svg')
+      .col-lg-10
+        .h5 Elementos de canales de distribución
+        p Conocer la penetración y la presencia en el mercado del competidor.  
+    hr.hr
+    .row.align-items-center
+      .col-lg-2
+        img.img-a(src='@/assets/curso/tema_6/img_11.svg')
+      .col-lg-10
+        .h5 Elementos de marca y actividades de punto de venta
+        p Analizar las acciones de activación e impulso de marcas de la competencia en diferentes puntos de venta y en las estrategias de consumo masivo.  
+    hr.hr
+    .row.align-items-center
+      .col-lg-2
+        img.img-a(src='@/assets/curso/tema_6/img_12.svg')
+      .col-lg-10
+        .h5 Elementos con el nivel de recomendación (NPS)
+        p Medir la lealtad y  la satisfacción del cliente permite generar mejores prácticas en términos de experiencias.
+
+     
+
+
+      
+
+</template>
+
+<script>
+export default {
+  name: 'Tema6',
+  data: () => ({
+    // variables de vue
+  }),
+}
+</script>
+
+<style lang="sass" scoped></style>
